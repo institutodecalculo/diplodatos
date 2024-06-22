07cfd47713d2f197db6f6d5126416b8350da4818
david weil <david.weil@endlesstruction.com.ar>
2024-06-22 16:22:47 -0300

    fix: script for navbar adaptation

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(function() {
-  $("main h1, main h2, main h3").each(function(){
+  $("h1, h2, h3").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
     $("nav ul li:first-child a").parent().addClass("active");
